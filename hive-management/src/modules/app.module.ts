ef8bb3568987823a47227c3c2da4274ea14833a5
patlee12 <patrick.f.lee93@gmail.com>
2024-05-09 23:22:46 -0400

    remove un used code

--- hive-management/src/modules/app.module.ts
+++ hive-management/src/modules/app.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from '../controllers/app.controller';
-import { AppService } from '../services/app.service';
 import { PrismaModule } from './prisma.module';
 import { UsersModule } from 'src/users/users.module';
 import { PostsModule } from 'src/posts/posts.module';
 
 @Module({
   imports: [PrismaModule, UsersModule, PostsModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
