21df2eda2ee8dd66a1056478c53ca274dbc37749
patlee12 <patrick.f.lee93@gmail.com>
2024-07-30 14:17:52 -0400

    remove comment

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -16,7 +16,7 @@ export const jwtSecret = process.env.JWT_SECRET;
     PassportModule,
     JwtModule.register({
       secret: jwtSecret,
-      signOptions: { expiresIn: '5m' }, // e.g. 30s, 7d, 24h
+      signOptions: { expiresIn: '5m' },
     }),
     UsersModule,
   ],
