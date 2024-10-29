268d130c5d759e251fc4e47bd8df75fd24715136
patlee12 <patrick.f.lee93@gmail.com>
2024-10-28 23:59:38 -0400

    add create function to roles permission service

--- src/app modules/user-management.module.ts
+++ src/app modules/user-management.module.ts
@@ -3,9 +3,16 @@ import { PrismaModule } from '../prisma/prisma.module';
 import { UsersModule } from 'src/users/users.module';
 import { PostsModule } from 'src/posts/posts.module';
 import { AuthModule } from 'src/auth/auth.module';
+import { RolesAndPermissionsModule } from 'src/roles-and-permissions/roles-and-permissions.module';
 
 @Module({
-  imports: [PrismaModule, UsersModule, PostsModule, AuthModule],
+  imports: [
+    PrismaModule,
+    UsersModule,
+    PostsModule,
+    AuthModule,
+    RolesAndPermissionsModule,
+  ],
   controllers: [],
   providers: [],
 })
--- src/roles-and-permissions/roles-and-permissions.service.ts
+++ src/roles-and-permissions/roles-and-permissions.service.ts
@@ -1,11 +1,29 @@
 import { Injectable } from '@nestjs/common';
-import { CreateRolesAndPermissionDto } from './dto/create-role.dto';
-import { UpdateRolesAndPermissionDto } from './dto/update-role.dto';
+import { CreateRoleDto } from './dto/create-role.dto';
+import { CreatePermissionDto } from './dto/create-permission.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { UpdateRoleDto } from './dto/update-role.dto';
 
 @Injectable()
 export class RolesAndPermissionsService {
-  create(createRolesAndPermissionDto: CreateRolesAndPermissionDto) {
-    return 'This action adds a new rolesAndPermission';
+  constructor(private prisma: PrismaService) {}
+
+  async createRole(createRoleDto: CreateRoleDto) {
+    const createRoleData = {
+      ...createRoleDto,
+      permissions: {
+        create: createRoleDto.permissions?.map(
+          (permission: CreatePermissionDto) => ({
+            name: permission.name,
+            description: permission.description,
+          }),
+        ),
+      },
+    };
+
+    return this.prisma.role.create({
+      data: createRoleData,
+    });
   }
 
   findAll() {
@@ -16,8 +34,8 @@ export class RolesAndPermissionsService {
     return `This action returns a #${id} rolesAndPermission`;
   }
 
-  update(id: number, updateRolesAndPermissionDto: UpdateRolesAndPermissionDto) {
-    return `This action updates a #${id} rolesAndPermission`;
+  update(id: number, UpdateRoleDto: UpdateRoleDto) {
+    return UpdateRoleDto;
   }
 
   remove(id: number) {
