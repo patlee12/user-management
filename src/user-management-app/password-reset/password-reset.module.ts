e15281acb0998bedfc494637d89c00ca32c4466e
patlee12 <patrick.f.lee93@gmail.com>
2025-03-10 02:19:40 -0400

    update module imports for roles guard

--- src/user-management-app/password-reset/password-reset.module.ts
+++ src/user-management-app/password-reset/password-reset.module.ts
@@ -3,10 +3,15 @@ import { PasswordResetService } from './password-reset.service';
 import { PasswordResetController } from './password-reset.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { UsersService } from '../users/users.service';
+import { RolesPermissionsResourcesService } from '../roles-and-permissions-resources/roles-permissions-resources.service';
 
 @Module({
   controllers: [PasswordResetController],
-  providers: [PasswordResetService, UsersService],
+  providers: [
+    PasswordResetService,
+    UsersService,
+    RolesPermissionsResourcesService,
+  ],
   imports: [PrismaModule],
 })
 export class PasswordResetModule {}
