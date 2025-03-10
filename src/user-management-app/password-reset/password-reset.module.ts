5022edd565030d8d0486622e3a6cf48a5ccf7864
patlee12 <patrick.f.lee93@gmail.com>
2025-03-10 02:05:42 -0400

    add service to module for password reset

--- src/user-management-app/password-reset/password-reset.module.ts
+++ src/user-management-app/password-reset/password-reset.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { PasswordResetService } from './password-reset.service';
 import { PasswordResetController } from './password-reset.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { UsersService } from '../users/users.service';
 
 @Module({
   controllers: [PasswordResetController],
-  providers: [PasswordResetService],
+  providers: [PasswordResetService, UsersService],
   imports: [PrismaModule],
 })
 export class PasswordResetModule {}
