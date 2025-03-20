32a1e253f2b32e02c4d9a2e0b08088c00a25b703
patlee12 <patrick.f.lee93@gmail.com>
2025-03-20 19:44:50 -0400

    fix type script and env loading for backend

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -10,6 +10,27 @@ import { PrismaClientExceptionFilter } from './prisma-client-exception/prisma-cl
 import { runAdminSeed } from './run-admin-seed';
 import { runMigrations } from './run-migrations';
 import * as os from 'os';
+import * as dotenv from 'dotenv';
+
+// Load the root .env file
+dotenv.config({ path: '../../.env' });
+
+// Load the backend .env file
+dotenv.config({ path: './.env' });
+
+// Perform substitutions for the database url
+const databaseUrl = process.env.DATABASE_URL?.replace(
+  '${POSTGRES_USER}',
+  process.env.POSTGRES_USER || 'default_user',
+)
+  .replace(
+    '${POSTGRES_PASSWORD}',
+    process.env.POSTGRES_PASSWORD || 'default_password',
+  )
+  .replace('${POSTGRES_DB}', process.env.POSTGRES_DB || 'default_db');
+
+// Update the DATABASE_URL environment variable for later use
+process.env.DATABASE_URL = databaseUrl || process.env.DATABASE_URL;
 
 async function bootstrap() {
   const logger = new Logger('Bootstrap');
