438fd896533c42620619080cd708b15290c08489
patlee12 <patrick.f.lee93@gmail.com>
2025-04-06 23:02:24 -0400

    comments

--- apps/frontend/homepage-app/app/services/authService.ts
+++ apps/frontend/homepage-app/app/services/authService.ts
@@ -7,12 +7,12 @@ import {
 } from '@user-management/types';
 
 /**
- * Sends a login request to the backend using email and password.
+ * Sends a login POST request to the backend `/auth/login` using email and password.
  * If MFA is enabled on the account, the response will include a temporary `ticket`
  * and `mfaRequired: true` instead of an access token.
  *
  * @param loginDto - The login data (email and password).
- * @returns A promise resolving to either an access token or MFA challenge response.
+ * @returns {Promise<AuthResponseDto>}A promise resolving to either an access token or MFA challenge response.
  */
 export async function login(loginDto: LoginDto): Promise<AuthResponseDto> {
   const res = await axiosInstance.post<AuthResponseDto>(
@@ -23,11 +23,11 @@ export async function login(loginDto: LoginDto): Promise<AuthResponseDto> {
 }
 
 /**
- * Submits a 6-digit MFA token along with a temporary ticket
+ * Submits POST request to `/auth/verify-mfa` with a 6-digit MFA token along with a temporary ticket
  * to complete the authentication process after login.
  *
  * @param mfaDto - The MFA token and ticket from the previous login step.
- * @returns A promise resolving to the final access token if verification succeeds.
+ * @returns {Promise<AuthResponseDto>} A promise resolving to the final access token if verification succeeds.
  */
 export async function verifyMfa(mfaDto: MfaDto): Promise<AuthResponseDto> {
   const res = await axiosInstance.post<AuthResponseDto>(
