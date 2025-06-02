503bec08c8281a5f8bb2b4be7529f01cb66e5b6c
patlee12 <patrick.f.lee93@gmail.com>
2025-06-02 13:08:10 -0400

    update shared lib to make a build and output to dist

--- libs/shared/src/components/terms-prompt.tsx
+++ libs/shared/src/components/terms-prompt.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useState } from 'react';
-import { CURRENT_TERMS_VERSION } from '../constants';
+import { CURRENT_TERMS_VERSION } from '../constants/constants';
 
 type TermsPromptProps = {
   /**
